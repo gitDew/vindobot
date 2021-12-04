ff6861d2613c4e7fdbcae60d63df855d69e781ac
gitDew <krisztian.harmat@gmail.com>
2021-12-04 16:44:18 +0100

    Add updater for updating sheets with workitout values

---
+++ tests/unit/test_updater.py
@@ -0,0 +1,65 @@
+import pytest
+import unittest
+from unittest.mock import MagicMock
+from updater import Updater
+
+def test_diff_and_new():
+    students = [
+            {"StammNr": "123456", "FirstName": "Cunci", "LastName": "Mokus", 
+                "RoomNr": "999", "BlockedTill": "2021-12-31 00:00:00"},
+            {"StammNr": "20000", "FirstName": "Janos", "LastName": "Pal", 
+                "RoomNr": "777", "BlockedTill": "0000-00-00 00:00:00"},
+            {"StammNr": "123460", "FirstName": "Bunci", "LastName": "Marrone", 
+                "RoomNr": "666", "BlockedTill": "0000-00-00 00:00:00"}
+            ]
+
+    entries = [
+            {"RowID": 100, "StammNr":"123456", "FirstName": "John", "LastName": "Serious", "RoomNr": "999", "BlockedTill": "2021-12-31 00:00:00", "From":"01.01.2022", "To":"01.02.2022", "Comment": "Serious guy"},
+            {"RowID": 102, "StammNr":"123460", "FirstName": "Jane", "LastName": "Marrone", "RoomNr": "120", "BlockedTill": "0000-00-00 00:00:00", "From":"", "To":"", "Comment": ""}
+            ]
+
+    fetcher = MagicMock()
+    reader = MagicMock()
+
+    fetcher.fetch.return_value = students
+    reader.getAllEntries.return_value = entries
+
+    updater = Updater(fetcher, reader, None) 
+
+    diff, new = updater.diff()
+    assert diff == [{"RowID": 100, "diff": [None, "Cunci", "Mokus", None, None]},
+                    {"RowID": 102, "diff": [None, "Bunci", None, "666", None]}]
+    
+    assert new == [{"StammNr": "20000", "FirstName": "Janos", "LastName": "Pal", 
+                "RoomNr": "777", "BlockedTill": "0000-00-00 00:00:00"}]
+
+def test_diff():
+    students = [
+            {"StammNr": "123456", "FirstName": "Cunci", "LastName": "Mokus", 
+                "RoomNr": "999", "BlockedTill": "2021-12-31 00:00:00"},
+            {"StammNr": "20000", "FirstName": "John", "LastName": "Paul", 
+                "RoomNr": "777", "BlockedTill": "0000-00-00 00:00:00"},
+            {"StammNr": "123460", "FirstName": "Bunci", "LastName": "Marrone", 
+                "RoomNr": "666", "BlockedTill": "0000-00-00 00:00:00"}
+            ]
+
+    entries = [
+            {"RowID": 100, "StammNr":"123456", "FirstName": "John", "LastName": "Serious", "RoomNr": "999", "BlockedTill": "2021-12-31 00:00:00", "From":"01.01.2022", "To":"01.02.2022", "Comment": "Serious guy"},
+            {"RowID": 101, "StammNr":"20000", "FirstName": "Janos", "LastName": "Pal", "RoomNr": "777", "BlockedTill": "0000-00-00 00:00:00", "From":"", "To":"", "Comment": "literally the pope"},
+            {"RowID": 102, "StammNr":"123460", "FirstName": "Jane", "LastName": "Marrone", "RoomNr": "120", "BlockedTill": "0000-00-00 00:00:00", "From":"", "To":"", "Comment": ""}
+            ]
+
+    fetcher = MagicMock()
+    reader = MagicMock()
+
+    fetcher.fetch.return_value = students
+    reader.getAllEntries.return_value = entries
+
+    updater = Updater(fetcher, reader, None) 
+
+    diff, new = updater.diff()
+    assert diff == [{"RowID": 100, "diff": [None, "Cunci", "Mokus", None, None]},
+                    {"RowID": 101, "diff": [None, "John", "Paul", None, None]},
+                    {"RowID": 102, "diff": [None, "Bunci", None, "666", None]}]
+    
+    assert new == []
---
+++ updater.py
@@ -0,0 +1,33 @@
+
+class Updater:
+    
+    def __init__(self, workitout_fetcher, google_reader, google_writer):
+        self.workitout_fetcher = workitout_fetcher
+        self.google_reader = google_reader
+        self.google_writer = google_writer
+    
+    def diff(self):
+        changes = []
+        new = []
+
+        wi_students = self.workitout_fetcher.fetch()
+        g_entries = self.google_reader.getAllEntries()
+        
+        by_stamm_nr = {student["StammNr"]: student for student in g_entries} 
+
+        for wi_student in wi_students:
+            student_changes = []
+            if wi_student["StammNr"] not in by_stamm_nr:
+                # new student who is not in sheets yet
+                new.append(wi_student) 
+                continue
+            entry = by_stamm_nr[wi_student["StammNr"]]
+            
+            for key in wi_student.keys():
+                if wi_student[key] != entry[key]:
+                    student_changes.append(wi_student[key])
+                else:
+                    student_changes.append(None)
+            if any(student_changes):
+                changes.append({"RowID": entry["RowID"], "diff": student_changes})
+        return changes, new

00630f16a0a0230360b627e6071a99a86e9f1543
gitDew <krisztian.harmat@gmail.com>
2021-12-05 11:20:26 +0100

    Move some commonly used fixtures to conftest.py

--- tests/integration/test_google.py
+++ tests/integration/test_google.py
@@ -1,28 +1,5 @@
-from googleapiclient.discovery import build
-import google_sheets
 import pytest
 from credentials import MyCredentials
-
-
-@pytest.fixture()
-def sheet_values_obj():
-    creds = google_sheets.authenticate()
-    service = build('sheets', 'v4', credentials=creds)
-    sheet_values_obj = service.spreadsheets().values()
-    return sheet_values_obj
-
-@pytest.fixture()
-def request_factory(sheet_values_obj):
-    spreadsheet_ID = "1Du82nqXiMQR1aQ5k_vzTvCkgDYthT8-e9HGi-_JfY_c" 
-    return google_sheets.RequestFactory(sheet_values_obj, spreadsheet_ID)
-
-@pytest.fixture()
-def reader(request_factory):
-    return google_sheets.Reader(request_factory)
-
-@pytest.fixture()
-def writer(request_factory):
-    return google_sheets.Writer(request_factory)
 
 def test_fetching(reader):
     values = reader.read("A1:H1")

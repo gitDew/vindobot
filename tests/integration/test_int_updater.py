7d545d0c1b44638896963299b5b04e9249f59fb5
gitDew <krisztian.harmat@gmail.com>
2021-12-05 11:22:16 +0100

    Add integration tests for updater diff

---
+++ tests/integration/test_int_updater.py
@@ -0,0 +1,133 @@
+import pytest
+import unittest
+from csv import DictReader
+from unittest.mock import MagicMock
+from updater import Updater
+
+@pytest.fixture
+def example_students():
+    return [{'StammNr': '1100650',
+      'FirstName': 'John',
+      'LastName': 'Lennon',
+      'RoomNr': '305',
+      'BlockedTill': ''},
+     {'StammNr': '1408000',
+      'FirstName': 'Bon',
+      'LastName': 'Jovi',
+      'RoomNr': '712',
+      'BlockedTill': ''},
+     {'StammNr': '1601200',
+      'FirstName': 'Keith',
+      'LastName': 'Richard',
+      'RoomNr': '608',
+      'BlockedTill': ''},
+     {'StammNr': '1701000',
+      'FirstName': 'Alice',
+      'LastName': 'Cooper',
+      'RoomNr': '704',
+      'BlockedTill': ''},
+     {'StammNr': '1703123',
+      'FirstName': 'Axl',
+      'LastName': 'Rose',
+      'RoomNr': '515',
+      'BlockedTill': ''}]
+
+@pytest.fixture
+def mock_fetcher(example_students):
+    fetcher = MagicMock()
+    fetcher.fetch.return_value = example_students
+    return fetcher
+
+@pytest.fixture
+def updater(mock_fetcher, reader):
+    return Updater(mock_fetcher, reader, None) 
+
+def test_given_unchanged_diff_is_empty(updater):
+    changes, new = updater.diff()
+
+    assert changes == []
+    assert new == []
+
+def test_given_new_student_only_new_has_stuff(updater, example_students):
+    example_students.append({'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''})
+    changes, new = updater.diff()
+
+    assert changes == []
+    assert new == [{'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''}]
+
+def test_given_existing_student_with_new_data_only_they_get_updated(updater, example_students):
+    example_students[3]["LastName"] = "Wonderland"
+    example_students[3]["RoomNr"] = "666"
+
+    changes, new = updater.diff()
+
+    assert new == []
+    assert changes == [{"RowID": 5, "diff": [None, None, "Wonderland", "666", None]}]
+
+def test_given_update_and_new_student_both_get_added(updater, example_students):
+    example_students.append({'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''})
+
+    example_students[3]["LastName"] = "Wonderland"
+    example_students[3]["RoomNr"] = "666"
+
+    changes, new = updater.diff()
+
+    assert new == [{'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''}]
+    assert changes == [{"RowID": 5, "diff": [None, None, "Wonderland", "666", None]}]
+
+def test_given_multiple_changes_all_get_added(updater, example_students):
+    example_students[3]["LastName"] = "Wonderland"
+    example_students[3]["RoomNr"] = "666"
+    example_students[0]["RoomNr"] = "123"
+    example_students[4]["FirstName"] = "Jesus"
+
+    changes, new = updater.diff()
+
+    assert new == []
+    assert changes == [
+            {"RowID": 2, "diff": [None, None, None, "123", None]},
+            {"RowID": 5, "diff": [None, None, "Wonderland", "666", None]},
+            {"RowID": 6, "diff": [None, "Jesus", None, None, None]}
+            ]
+
+def test_given_multiple_new_all_get_added(updater, example_students):
+    example_students.append({'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''})
+    example_students.append({'StammNr': '7777777',
+      'FirstName': 'Lucky',
+      'LastName': 'Man',
+      'RoomNr': '777',
+      'BlockedTill': '24.12.2022'})
+    changes, new = updater.diff()
+
+    assert changes == []
+    assert new == [{'StammNr': '0000007',
+      'FirstName': 'James',
+      'LastName': 'Bond',
+      'RoomNr': '007',
+      'BlockedTill': ''},
+      {'StammNr': '7777777',
+      'FirstName': 'Lucky',
+      'LastName': 'Man',
+      'RoomNr': '777',
+      'BlockedTill': '24.12.2022'}
+      ]

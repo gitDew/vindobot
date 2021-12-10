0d20c1f50ea28db3bd453a082820375e548ba1e0
gitDew <krisztian.harmat@gmail.com>
2021-12-10 02:18:05 +0100

    Some commonly used fixtures only per module

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,22 @@ import pytest
 from googleapiclient.discovery import build
 import google_sheets
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def sheet_values_obj():
     creds = google_sheets.authenticate()
     service = build('sheets', 'v4', credentials=creds)
     sheet_values_obj = service.spreadsheets().values()
     return sheet_values_obj
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def request_factory(sheet_values_obj):
     spreadsheet_ID = "1Du82nqXiMQR1aQ5k_vzTvCkgDYthT8-e9HGi-_JfY_c" 
     return google_sheets.RequestFactory(sheet_values_obj, spreadsheet_ID)
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def reader(request_factory):
     return google_sheets.Reader(request_factory)
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def writer(request_factory):
     return google_sheets.Writer(request_factory)

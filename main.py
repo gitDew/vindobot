6931c2aeeef9f4993adbcf524de0ec8810fea33d
gitDew <krisztian.harmat@gmail.com>
2022-02-16 15:56:24 +0100

    More around some stuff in main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
-import google_sheets
-from credentials import MyCredentials
-from student import Student
-from blocker import Blocker
-from updater import Updater
-
 from chromedriver_py import binary_path
 from googleapiclient.discovery import build
 from selenium import webdriver
-import workitout
 
-spreadsheet_ID = "16p4zH-z6z2ggr86Vmson9mlroWhWE85zECgryrv2Vno" 
+import google_sheets
+import workitout
+from student import Student
+from blocker import Blocker
+from updater import Updater
+from credentials import MyCredentials
+
 
 creds = google_sheets.authenticate()
 service = build('sheets', 'v4', credentials=creds)
 sheet_values_obj = service.spreadsheets().values()
 
+spreadsheet_ID = "16p4zH-z6z2ggr86Vmson9mlroWhWE85zECgryrv2Vno" 
 request_factory = google_sheets.RequestFactory(sheet_values_obj, spreadsheet_ID)
 greader = google_sheets.Reader(request_factory, "Heimbewohner Liste")
 gwriter = google_sheets.Writer(request_factory, "Heimbewohner Liste")
